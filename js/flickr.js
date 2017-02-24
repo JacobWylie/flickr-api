d3fcd82866bbf7337b1e2e27a6ac48c94200ac36
JacobWylie <jacobwylie@gmail.com>
2017-02-24 19:06:07 +0100

    Added search feature

--- js/flickr.js
+++ js/flickr.js
@@ -21,3 +21,33 @@ $('button').click(function() {
 	}
 	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
 }); // End Button Click Event
+
+// Form search event for #tag selections
+$('form').submit(function(evt) {
+	evt.preventDefault();
+	var $searchField = $('#search');
+	var $submitButton = $('#submit');
+
+	$searchField.prop("disabled", true);
+	$submitButton.attr("disabled", true).val("searching...");
+	var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var tag = $searchField.val();
+	tag += ",berlin,black,white";
+	var flickrOptions = {
+		tags: tag,
+		format: "json"
+	};
+	function displayPhotos(data) {
+		var photoHTML = '<ul>';
+		$.each( data.items, function(i, photo) {
+			photoHTML += '<li class="grid-25 tablet-grid-50">';
+			photoHTML += '<a href="' + photo.link + '" class="image">';
+			photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+		});
+		photoHTML += '</ul>';
+		$('#photos').html(photoHTML);
+		$searchField.prop("disabled", false);
+		$submitButton.attr("disabled", false).val("Search");
+	}
+	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
+}); // End Form Search Event
