91dd3081a779e0738a622181eca7252484040c08
JacobWylie <jacobwylie@gmail.com>
2017-02-24 17:01:48 +0100

    Added AJAX request

--- js/flickr.js
+++ js/flickr.js
@@ -1,4 +1,9 @@
+// Button click event for #tag selections
 $('button').click(function() {
 	$('button').removeClass("selected");
 	$(this).addClass("selected");
+	var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var tag = $(this).text()
+	tag += ",berlin"
+	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
 }); // End Button Click Event
