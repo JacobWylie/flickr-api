b777a996fe93d13c9ef50364c8096738ee5f8d7c
JacobWylie <jacobwylie@gmail.com>
2017-02-24 17:30:03 +0100

    Added function displayPhotos() to implement HTML

--- js/flickr.js
+++ js/flickr.js
@@ -2,8 +2,22 @@
 $('button').click(function() {
 	$('button').removeClass("selected");
 	$(this).addClass("selected");
-	var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-	var tag = $(this).text()
-	tag += ",berlin"
+	var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var tag = $(this).text();
+	tag += ",berlin,black,white";
+	var flickrOptions = {
+		tags: tag,
+		format: "json"
+	};
+	function displayPhotos(data) {
+		var photoHTML = '<ul>';
+		$.each( data.items, function(i, photo) {
+			photoHTML += '<li class="grid-25 tablet-grid-50">';
+			photoHTML += '<a href="' + photo.link + '" class="image">';
+			photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+		});
+		photoHTML += '</ul>';
+		$('#photos').html(photoHTML);
+	}
 	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
 }); // End Button Click Event
