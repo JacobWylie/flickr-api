516b8a05086a3652598ab8ff83432f787e204d36
JacobWylie <jacobwylie@gmail.com>
2017-06-13 22:04:33 +0200

    refactored

--- js/main.js
+++ js/main.js
@@ -1,45 +1,10 @@
-// Button click event for #tag selections
-$('button').click(function() {
-	$('button').removeClass("selected");
-	$(this).addClass("selected");
-	var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-	var tag = $(this).text();
-	var $searchField = $('#search');
-	$searchField.attr('placeholder', tag);
-	tag += ",berlin,blackandwhite";
-	var flickrOptions = {
-		tags: tag,
-		format: "json"
-	};
-	function displayPhotos(data) {
-		var photoHTML = '<ul>';
-		$.each( data.items, function(i, photo) {
-			var image = (photo.media.m).replace('_m.jpg', '_b.jpg');
-			photoHTML += '<li class="grid-25 tablet-grid-50">';
-			photoHTML += `<a href="${image}" data-lightbox="image" class="image">`;
-			photoHTML += `<img src="${photo.media.m}"></a></li>`;
-		});
-		photoHTML += '</ul>';
-		$('#photos').html(photoHTML);
-	}
-	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
-}); // End Button Click Event
+let flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+let tag;
+let flickrOptions = {};
+const $searchField = $('#search');
+const $submitButton = $('#submit');
 
-// Form search event for #tag selections
-$('form').submit(function(evt) {
-	evt.preventDefault();
-	var $searchField = $('#search');
-	var $submitButton = $('#submit');
-	$searchField.prop("disabled", true);
-	$submitButton.attr("disabled", true).val("searching...");
-	var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-	var tag = $searchField.val();
-	tag += ",berlin,black and white";
-	var flickrOptions = {
-		tags: tag,
-		format: "json"
-	};
-	function displayPhotos(data) {
+function displayPhotos(data) {
 		var photoHTML = '<ul>';
 		$.each( data.items, function(i, photo) {
 			var image = (photo.media.m).replace('_m.jpg', '_b.jpg');
@@ -52,11 +17,35 @@ $('form').submit(function(evt) {
 		$searchField.prop("disabled", false);
 		$submitButton.attr("disabled", false).val("Search");
 	}
+
+// Button click event for #tag selections
+$('button').click(function() {
+	tag = $(this).text();
+	$searchField.attr('placeholder', tag);
+	tag += ",berlin,blackandwhite";
+	flickrOptions = {
+		tags: tag,
+		format: "json"
+	}
+	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
+}); // End Button Click Event
+
+// Form search event for #tag selections
+$('form').submit(function(evt) {
+	evt.preventDefault();
+	$searchField.prop("disabled", true);
+	$submitButton.attr("disabled", true).val("searching...");
+	tag = $searchField.val();
+	tag += ",berlin,black and white";
+	flickrOptions = {
+		tags: tag,
+		format: "json"
+	}
 	$.getJSON(flickrAPI, flickrOptions, displayPhotos);
 
 }); // End Form Search Event
-// Lightbox
 
+// Lightbox
 lightbox.option({
 })
 
